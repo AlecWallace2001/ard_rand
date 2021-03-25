1ae2cea92a399486aad32e84a0b52054b26b28b0
AlecWallace2001 <leebratina@gmail.com>
2021-03-25 01:44:04 -0500

    Update sketch_mar_24a.ino
    updated with most of the remaining logic. Just need to sort out why validation isnt working.

--- sketch_mar_24a.ino
+++ sketch_mar_24a.ino
@@ -1,10 +1,3 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  
-}
-
 String start (int a)
 {
   String binary  ("");
@@ -21,16 +14,25 @@ String start (int a)
  };
 
 
-  String bin1 = start((rand() %30)+1);
-  String bin2 = start((rand() %30)+1);
-  String bin3 = start((rand() %30)+1);
-  String bin4 = start((rand() %30)+1);
-  String bin5 = start((rand() %30)+1);
-  String bin6 = start((rand() %30)+1);
-  String bin7 = start((rand() %30)+1);
-  String bin8 = start((rand() %30)+1);
-  String bin9 = start((rand() %30)+1);
-  
+  String bin1 = start((rand() %29)+1);
+  String bin2 = start((rand() %29)+1);
+  String bin3 = start((rand() %29)+1);
+  String bin4 = start((rand() %29)+1);
+  String bin5 = start((rand() %29)+1);
+  String bin6 = start((rand() %29)+1);
+  String bin7 = start((rand() %29)+1);
+  String bin8 = start((rand() %29)+1);
+  String bin9 = start((rand() %29)+1);
+
+  int attempt[10] ={0,0,0,0,0,0,0,0,0,0};
+  int next_attempt[10] ={2,2,2,2,2,2,2,2,2,2};
+  int solution[5] ={1,1,1,1,1};
+  int entry[5] ={0,0,0,0,0};
+  int a_state = 0;
+  int b_state = 0;
+  int c_state = 0;
+  int valid_sol = 0;
+  int curr_state[5] ={0,0,0,0,0};
   int rotary0Seq[][5] = {
     {bin1[0]-'0',bin1[1]-'0',bin1[2]-'0',bin1[3]-'0',bin1[4]-'0'},
     {bin2[0]-'0',bin2[1]-'0',bin2[2]-'0',bin2[3]-'0',bin2[4]-'0'},
@@ -46,6 +48,30 @@ String start (int a)
     {bin8[0]-'0',bin8[1]-'0',bin8[2]-'0',bin8[3]-'0',bin8[4]-'0'},
     {bin9[0]-'0',bin9[1]-'0',bin9[2]-'0',bin9[3]-'0',bin9[4]-'0'}
   };
+  int solutionP1[][5] = {
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+  };
+  int solutionP2[][5] = {
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+    {0,0,0,0,0},
+  };
 
 
 int cycle ()
@@ -59,7 +85,6 @@ int cycle ()
   bin7 = start((rand() %30)+1);
   bin8 = start((rand() %30)+1);
   bin9 = start((rand() %30)+1);
-  
   rotary0Seq[0][0]= bin1[0]-'0';
   rotary0Seq[0][1]= bin1[1]-'0';
   rotary0Seq[0][2]= bin1[2]-'0';
@@ -75,7 +100,6 @@ int cycle ()
   rotary0Seq[2][2]= bin3[2]-'0';
   rotary0Seq[2][3]= bin3[3]-'0';
   rotary0Seq[2][4]= bin3[4]-'0';
-
   rotary1Seq[0][0]= bin4[0]-'0';
   rotary1Seq[0][1]= bin4[1]-'0';
   rotary1Seq[0][2]= bin4[2]-'0';
@@ -91,7 +115,6 @@ int cycle ()
   rotary1Seq[2][2]= bin6[2]-'0';
   rotary1Seq[2][3]= bin6[3]-'0';
   rotary1Seq[2][4]= bin6[4]-'0';
-  
   rotary2Seq[0][0]= bin7[0]-'0';
   rotary2Seq[0][1]= bin7[1]-'0';
   rotary2Seq[0][2]= bin7[2]-'0';
@@ -110,21 +133,171 @@ int cycle ()
  return 0;
  };
 
+int iterate_attempt() {
+  int j_var = 9;
+  do {
+    if (attempt[j_var]==2) {
+      attempt[j_var]=0;
+    } else {
+      attempt[j_var] = 1 + attempt[j_var];
+      return 0;
+    }
+    j_var--;
+  }
+  while (j_var >=0);
+  return 0;
+}
 
+int state_cycle(char var_state) {
+  if(var_state=='a') {
+    if(a_state==2) {
+      a_state = 0;
+    } else {
+      a_state = 1 + a_state;
+    }
+  } else if (var_state=='b') {
+    if(b_state==2) {
+      b_state = 0;
+    } else {
+      b_state = 1 + b_state;
+    }
+  } else {
+    if(c_state==2) {
+      c_state = 0;
+    } else {
+      c_state = 1 + c_state;
+    }
+  }
+};
 
+int next_state(int* state, int loc) {
+    Serial.print("current: "); 
+    Serial.print(curr_state[0]); 
+    Serial.print(curr_state[1]); 
+    Serial.print(curr_state[2]); 
+    Serial.print(curr_state[3]); 
+    Serial.println(curr_state[4]); 
+    Serial.print("add: "); 
+    Serial.print(state[0]); 
+    Serial.print(state[1]); 
+    Serial.print(state[2]); 
+    Serial.print(state[3]); 
+    Serial.println(state[4]); 
+  for (int i = 0; i <= 4; i++) {
+    if (state[i] + curr_state[i] ==2) {
+      curr_state[i] = 0;
+    } else {
+      curr_state[i] = state[i] + curr_state[i];
+    }
+  }
+}
+
+int validation(){
+  if(solution == curr_state) {
+    valid_sol = 1;
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+int gen_val_state() {
+  do {
+    do{
+        for (int i = 0; i <= 9; i++) {
+          if (attempt[i]==0) {
+            next_state(rotary0Seq[a_state],i);
+            state_cycle('a');
+          } else if (attempt[i]==1) {
+            next_state(rotary1Seq[b_state],i);
+            state_cycle('b');
+          } else {
+            next_state(rotary2Seq[c_state],i);
+            state_cycle('c');
+          }
+        }
+        
+        Serial.println("");
+        if (valid_sol==1){
+          return 0;
+        }
+        iterate_attempt();
+    }
+    while (attempt != next_attempt);
+    cycle();
+  }
+  while (valid_sol == 0);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  Serial.println("Start");
+  gen_val_state();
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
 int var = 0;
 
-Serial.println("Start");
-Serial.println(rotary0Seq[0][0]);
-do
-{
-  cycle();
-  var++;
-}
-while (var < 200);
+Serial.println(validation());
+Serial.println("Seg1");
+Serial.print(rotary0Seq[0][0]);
+Serial.print(rotary0Seq[0][1]);
+Serial.print(rotary0Seq[0][2]);
+Serial.print(rotary0Seq[0][3]);
+Serial.println(rotary0Seq[0][4]);
+Serial.print(rotary0Seq[1][0]);
+Serial.print(rotary0Seq[1][1]);
+Serial.print(rotary0Seq[1][2]);
+Serial.print(rotary0Seq[1][3]);
+Serial.println(rotary0Seq[1][4]);
+Serial.print(rotary0Seq[2][0]);
+Serial.print(rotary0Seq[2][1]);
+Serial.print(rotary0Seq[2][2]);
+Serial.print(rotary0Seq[2][3]);
+Serial.println(rotary0Seq[2][4]);
+Serial.println("Seg2");
+Serial.print(rotary1Seq[0][0]);
+Serial.print(rotary1Seq[0][1]);
+Serial.print(rotary1Seq[0][2]);
+Serial.print(rotary1Seq[0][3]);
+Serial.println(rotary1Seq[0][4]);
+Serial.print(rotary1Seq[1][0]);
+Serial.print(rotary1Seq[1][1]);
+Serial.print(rotary1Seq[1][2]);
+Serial.print(rotary1Seq[1][3]);
+Serial.println(rotary1Seq[1][4]);
+Serial.print(rotary1Seq[2][0]);
+Serial.print(rotary1Seq[2][1]);
+Serial.print(rotary1Seq[2][2]);
+Serial.print(rotary1Seq[2][3]);
+Serial.println(rotary1Seq[2][4]);
+Serial.println("Seg3");
+Serial.print(rotary2Seq[0][0]);
+Serial.print(rotary2Seq[0][1]);
+Serial.print(rotary2Seq[0][2]);
+Serial.print(rotary2Seq[0][3]);
+Serial.println(rotary2Seq[0][4]);
+Serial.print(rotary2Seq[1][0]);
+Serial.print(rotary2Seq[1][1]);
+Serial.print(rotary2Seq[1][2]);
+Serial.print(rotary2Seq[1][3]);
+Serial.println(rotary2Seq[1][4]);
+Serial.print(rotary2Seq[2][0]);
+Serial.print(rotary2Seq[2][1]);
+Serial.print(rotary2Seq[2][2]);
+Serial.print(rotary2Seq[2][3]);
+Serial.println(rotary2Seq[2][4]);
+
+Serial.println("Solution P1");
+//Serial.println(solutionP1);
+Serial.println("Solution P1");
+//Serial.println(solutionP2);
+Serial.println("solution list");
+//Serial.println(attempt);
+
 
 String command = start((rand() %30)+1);
 Serial.println(rotary0Seq[0][0]);
