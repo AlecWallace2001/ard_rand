f2e9343012b091846ca3c19f13b791133d962592
AlecWallace2001 <leebratina@gmail.com>
2021-04-05 01:01:57 -0500

    Update sketch_mar_24a.ino
    Expanded random logic to generate a more random seed when raw analog pin read only returns a single digit.
    Added delay to the random seed logic to resolve issue with memory poisoning.
    Added debug logic
    Set minimum/maximum solution to 5/7. Reaching a minimum solution of 5 takes between 8 - 120 seconds, averaging 12 seconds over tests. A solution of 6 takes 8 - 25 minutes, with an average of 16.

--- sketch_mar_24a.ino
+++ sketch_mar_24a.ino
@@ -1,6 +1,15 @@
+//Initializing the debug process. Setup to view the randomness generated due to an issue when reading
+//a0
+#ifdef DEBUG_ESP_PORT
+#define DEBUG_MSG(...) DEBUG_ESP_PORT.printf( __VA_ARGS__ )
+#else
+#define DEBUG_MSG(...)
+#endif
+
 String start (int a)
 {
   //This function converts a number between 1 and 31 to a binary string and returns it
+  //The binary int has to be converted character by character and accomodate zero padding.
   String binary  ("");
     int mask = 1;
     for(int i = 0; i < 5; i++)
@@ -17,9 +26,32 @@ String start (int a)
 int updateRand(){
   //This function generates the random seed to avoid duplicate sets
   long seed;
-  for (int i = 0; i <= 10; i++) {
-    seed = seed + (analogRead(0)*analogRead(0));
+  long temp0;
+  long temp1;
+  for (int i = 0; i <= 10; i++) { // analogRead(0) seems to respond better tha, A0. Using ESP8266
+                                  // with a bare analog pin, only a single digit (4-6) is created.
+                                  // randomizing the seed segments with the cubed read function
+                                  // generates a wider range of characters
+    temp0 = random(1,random(1,(analogRead(0)*analogRead(0)*analogRead(0))));
+    delay(1); //IMPORTANT Running the random generation too quickly causes memory to be exceeded and
+              //there is data poisoning. This delay slows the process down so this doesnt happen.
+              //This was discovered when the code worked without issue when running debug, but errored on
+              //raw runs.
+    temp1 = random(temp0,random(1,(temp0*analogRead(0)*analogRead(0)*analogRead(0))));
+    #ifdef DEBUG_ESP_PORT //This allows the debug to print a line of 
+    //Serial.print("(");
+    //Serial.print(i);
+    //Serial.print(")");
+    //Serial.print(temp0);
+    //Serial.print(" ");
+    //Serial.print(temp1);
+    //Serial.print(", ");
+    #endif
+    seed = seed + (temp0*temp1);
   };
+  #ifdef DEBUG_ESP_PORT
+  //Serial.println(" end");
+  #endif
   seed = seed*seed;
   randomSeed(seed);
   return 0;
@@ -40,40 +72,38 @@ int updateRand(){
   int print_cycle = 0;
   int prev = 4;
   int steps = 0;
-  int attempt[10] ={0,0,0,0,0,0,0,0,0,0};
-  int next_attempt[10] ={2,2,2,2,2,2,2,2,2,2};
-  int solution[5] ={1,1,1,1,1};
-  int entry[5] ={0,0,0,0,0};
-  int a_state = 0;
-  int b_state = 0;
-  int c_state = 0;
-  int usa = 0;
-  int usb = 0;
-  int usc = 0;
-  int valid_sol = 0;
-  int curr_state[5] ={0,0,0,0,0};
+  int attempt[10] ={0,0,0,0,0,0,0,0,0,0}; //current state being attempted
+  int next_attempt[10] ={2,2,2,2,2,2,2,2,2,2}; //array to compare against to generate an attempt based iteration
+  int solution[5] ={1,1,1,1,1}; // maintain comparative uniformity by using an expected array
+  int a_state = 0; //button state, 0,1, or 2
+  int b_state = 0; //button state, 0,1, or 2
+  int c_state = 0; //button state, 0,1, or 2
+  int valid_sol = 0; //Flag indicating if a valid solution has been found
+  int curr_state[5] ={0,0,0,0,0}; // Current state based on the latest step
   int buttonSeq[][5] = {
     {bin1[0]-'0',bin1[1]-'0',bin1[2]-'0',bin1[3]-'0',bin1[4]-'0'},
     {bin2[0]-'0',bin2[1]-'0',bin2[2]-'0',bin2[3]-'0',bin2[4]-'0'},
     {bin3[0]-'0',bin3[1]-'0',bin3[2]-'0',bin3[3]-'0',bin3[4]-'0'}
-  };
+  }; // Button 1 set
   int rotary1Seq[][5] = {
     {bin4[0]-'0',bin4[1]-'0',bin4[2]-'0',bin4[3]-'0',bin4[4]-'0'},
     {bin5[0]-'0',bin5[1]-'0',bin5[2]-'0',bin5[3]-'0',bin5[4]-'0'},
     {bin6[0]-'0',bin6[1]-'0',bin6[2]-'0',bin6[3]-'0',bin6[4]-'0'}
-  };
+  }; // Button 2 set
   int rotary2Seq[][5] = {
     {bin7[0]-'0',bin7[1]-'0',bin7[2]-'0',bin7[3]-'0',bin7[4]-'0'},
     {bin8[0]-'0',bin8[1]-'0',bin8[2]-'0',bin8[3]-'0',bin8[4]-'0'},
     {bin9[0]-'0',bin9[1]-'0',bin9[2]-'0',bin9[3]-'0',bin9[4]-'0'}
-  };
+  }; // Button 3 set
+  unsigned long myTime;// millis count to show how long to find the solution
+  unsigned long stTime;// millis count before generating the solution 
 
 
 int cycle ()
 {
   //This function generates a new set by selecting a new seed, creating new binary values,
   //then assigning the new values to the array
-  updateRand();
+  updateRand(); //Update the seed to reduce the probability of duplicate solutions
   prsteps = "";
   bin1 = start(random(1,30));
   bin2 = start(random(1,30));
@@ -105,9 +135,9 @@ int iterate_attempt() {
   int j_var = 0;
   do {
     if (attempt[j_var]==2) {
-      attempt[j_var]=0;
+      attempt[j_var]=0;// Iterate the current step and continue the loop
     } else {
-      attempt[j_var] = 1 + attempt[j_var];
+      attempt[j_var] = 1 + attempt[j_var];// iterate the current step and return
       return 0;
     }
     j_var++;
@@ -118,19 +148,19 @@ int iterate_attempt() {
 
 int state_cycle(char var_state) {
   //This cycles our state between the different program button presses.
-  if(var_state=='a') {
+  if(var_state=='a') {// Cycle the state if the first button was pressed using base 3
     if(a_state==2) {
       a_state = 0;
     } else {
       a_state = 1 + a_state;
     }
-  } else if (var_state=='b') {
+  } else if (var_state=='b') { // Cycle the state if the second button was pressed using base 3
     if(b_state==2) {
       b_state = 0;
     } else {
       b_state = 1 + b_state;
     }
-  } else {
+  } else { // Cycle the state if the third button was pressed using base 3
     if(c_state==2) {
       c_state = 0;
     } else {
@@ -147,9 +177,11 @@ int reset_state() {
   curr_state[2] = 0;
   curr_state[3] = 0;
   curr_state[4] = 0;
+  valid_sol = 0;
   a_state = 0;
   b_state = 0;
   c_state = 0;
+  prsteps = " ";
   return 0;
 };
 
@@ -169,25 +201,25 @@ int next_state(int* state, int loc) {
 int validation(int step_num){
   //This just compares our currrent state with the anticipated solution. If all values in the current
   //light state match the solution state, then the valid solution flag is set.
-  prsteps = prsteps + "current: "; 
-  prsteps = prsteps + curr_state[0]; 
-  prsteps = prsteps + curr_state[1]; 
-  prsteps = prsteps + curr_state[2]; 
-  prsteps = prsteps + curr_state[3]; 
-  prsteps = prsteps + curr_state[4]; 
+  prsteps = prsteps + "current: "; // This logic creates the output for visual validation of the solution
+  prsteps = prsteps + String(curr_state[0]); 
+  prsteps = prsteps + String(curr_state[1]); 
+  prsteps = prsteps + String(curr_state[2]); 
+  prsteps = prsteps + String(curr_state[3]); 
+  prsteps = prsteps + String(curr_state[4]); 
   prsteps = prsteps + ";"; 
   prsteps = prsteps + "solution: "; 
-  prsteps = prsteps + solution[0]; 
-  prsteps = prsteps + solution[1]; 
-  prsteps = prsteps + solution[2]; 
-  prsteps = prsteps + solution[3]; 
-  prsteps = prsteps + solution[4];
+  prsteps = prsteps + String(solution[0]); 
+  prsteps = prsteps + String(solution[1]); 
+  prsteps = prsteps + String(solution[2]); 
+  prsteps = prsteps + String(solution[3]); 
+  prsteps = prsteps + String(solution[4]);
   prsteps = prsteps + " ,"; 
   if((solution[0] == curr_state[0])&&(solution[1] == curr_state[1])&&(solution[2] == curr_state[2])&&(solution[3] == curr_state[3])&&(solution[4] == curr_state[4])) {
     if (valid_sol == 0) {
           steps = step_num+1;
     };
-    valid_sol = 1;
+    valid_sol = 1; //setting the global valid flag once a solution is found 
     return 1;
   } else {
     return 0;
@@ -225,14 +257,36 @@ int no_dupe() {
   prev = 4;
   steps;
   attempt;
-  for (int i = 0; i <= steps; i++) {
+  int st0 = 0;
+  int st1 = 0;
+  int st2 = 0;
+  for (int i = 0; i < steps; i++) { //This cycles through the steps in the solution to compare the 
+                                    //current step with the previous to detect duplicates
     if (attempt[i] == prev) {
       return 0;
     } else {
       prev = attempt[i];
     }
   }
+  for (int i = 0; i < steps; i++) { //This cycles through the attempts to verify that the solution 
+                                    //has used each button atleast once.
+  #ifdef DEBUG_ESP_PORT
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.println(attempt[i]);
+  #endif
+    if (attempt[i] == 0) {
+      st0++;
+    } else if (attempt[i] == 1) {
+      st1++;
+    } else if (attempt[i] == 2) {
+      st2++;
+    }
+  }
+  if ((st0>=1)&&(st1>=1)&&(st2>=1)) {
     return 1;
+  }
+    return 0;
 }
 
 int gen_val_state() {
@@ -260,7 +314,7 @@ int gen_val_state() {
           }
           validation(i);
         }
-        if ((valid_sol==1)&&(steps>=4)&&(steps<=7)&&(no_dupe()==1)) {
+        if ((valid_sol==1)&&(steps>=5)&&(steps<=7)&&(no_dupe()==1)) {
           printsteps();
           return 1;
         } else if ((valid_sol==1) && (steps>7) && (no_dupe()==0)) {
@@ -285,10 +339,16 @@ int gen_val_state() {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  //Serial1.begin(115200);
+  DEBUG_MSG("bootup...\n");
   pinMode(13, OUTPUT);
   Serial.println("Start");
+  stTime = millis();
   cycle();
   gen_val_state();
+  myTime = millis()-stTime;
+  Serial.print("generation in mills: ");
+  Serial.println(myTime);
 }
 
 void loop() {
@@ -327,4 +387,15 @@ if (print_cycle==0) {
 };
 print_cycle++;
 delay(500);
+
+  #ifdef DEBUG_ESP_PORT
+  reset_state();
+  Serial.println("Start");
+  stTime = millis();
+  cycle();
+  gen_val_state();
+  myTime = millis()-stTime;
+  Serial.print("generation in mills: ");
+  Serial.println(myTime);
+  #endif
 }
